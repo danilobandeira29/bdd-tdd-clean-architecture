377d6e393e75b1c966a7b8aeda8614eb14defaf2
Danilo Bandeira <danilobandeira29@gmail.com>
2021-01-21 16:47:30 -0300

    feat: add replace method in CacheRepositoryInterface

--- src/data/interfaces/cache/cache-repository.ts
+++ src/data/interfaces/cache/cache-repository.ts
@@ -1,6 +1,7 @@
 export interface CacheRepositoryInterface {
   delete: (key: string) => void
   save: (key: string, value: any) => void
+  replace: (key: string, value: any) => void
 }
 
 export namespace CacheRepositoryInterface {
